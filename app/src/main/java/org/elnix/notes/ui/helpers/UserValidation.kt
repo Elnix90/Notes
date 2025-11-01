5068ed2c361d1f465672065afeeae0edc5bc70d8
Elnix90 <elnix91@gmail.com>
2025-11-01 17:14:31 +0100

    Enhanced User validation prompt

--- app/src/main/java/org/elnix/notes/ui/helpers/UserValidation.kt
+++ app/src/main/java/org/elnix/notes/ui/helpers/UserValidation.kt
@@ -1,8 +1,11 @@
 package org.elnix.notes.ui.helpers
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
@@ -13,7 +16,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import org.elnix.notes.R
@@ -60,6 +65,7 @@ fun UserValidation(
             if (title != null) {
                 Row(
                     horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier.padding(bottom = 4.dp)
                 ) {
                     Row(

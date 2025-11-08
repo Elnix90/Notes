61ef607c6ad89425513e0a93ed15c8b951017372
Elnix90 <elnix91@gmail.com>
2025-11-08 17:39:35 +0100

    Also removed navBar fun un MainAppUi.kt

--- app/src/main/java/org/elnix/notes/MainAppUi.kt
+++ app/src/main/java/org/elnix/notes/MainAppUi.kt
@@ -1,42 +1,25 @@
 package org.elnix.notes
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.FormatListBulleted
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import org.elnix.notes.data.helpers.NoteType
-import org.elnix.notes.data.settings.ShowNavBarActions
-import org.elnix.notes.data.settings.stores.UiSettingsStore
 import org.elnix.notes.ui.NoteViewModel
 import org.elnix.notes.ui.editors.ChecklistEditorScreen
 import org.elnix.notes.ui.editors.DrawingEditorScreen
 import org.elnix.notes.ui.editors.NoteEditorScreen
 import org.elnix.notes.ui.security.LockScreen
-import org.elnix.notes.ui.theme.adjustBrightness
 
 // -------------------- ROUTES --------------------
 object Routes {
@@ -176,54 +159,3 @@ fun NavGraphBuilder.settingsNavGraph(navController: NavHostController, vm: NoteV
         composable(Routes.Settings.DebugSub.OTHER) { OtherSettingsScreen(navController) }
     }
 }
-
-// -------------------- BOTTOM NAV --------------------
-@Composable
-fun BottomNav(navController: NavHostController) {
-    val ctx = LocalContext.current
-    val showNavbarLabel by UiSettingsStore.getShowBottomNavLabelsFlow(ctx)
-        .collectAsState(initial = ShowNavBarActions.ALWAYS)
-
-    val items = listOf(
-        BottomNavItem(Routes.NOTES, stringResource(R.string.notes), Icons.AutoMirrored.Filled.FormatListBulleted),
-        BottomNavItem(Routes.Settings.ROOT, stringResource(R.string.settings), Icons.Default.Settings)
-    )
-
-    NavigationBar(containerColor = MaterialTheme.colorScheme.surface) {
-        val current = navController.currentBackStackEntryAsState().value?.destination?.route.orEmpty()
-
-        items.forEach { item ->
-            val isCurrentOrSub = current.startsWith(item.route)
-            val showLabel = when (showNavbarLabel) {
-                ShowNavBarActions.ALWAYS -> true
-                ShowNavBarActions.NEVER -> false
-                ShowNavBarActions.SELECTED -> isCurrentOrSub
-                ShowNavBarActions.OTHER -> !isCurrentOrSub
-            }
-
-            NavigationBarItem(
-                selected = current.startsWith(item.route),
-                onClick = {
-                    if( current != item.route ) {
-                        navController.navigate(item.route) { launchSingleTop = true }
-                    }
-                },
-                icon = { Icon(item.icon, contentDescription = item.label) },
-                label = if (!showLabel) null else { { Text(item.label, color = MaterialTheme.colorScheme.onBackground) } },
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = MaterialTheme.colorScheme.primary.adjustBrightness(1.5f),
-                    unselectedIconColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
-                    selectedTextColor = MaterialTheme.colorScheme.primary,
-                    unselectedTextColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.3f),
-                    indicatorColor = Color.Transparent
-                )
-            )
-        }
-    }
-}
-
-data class BottomNavItem(
-    val route: String,
-    val label: String,
-    val icon: androidx.compose.ui.graphics.vector.ImageVector
-)

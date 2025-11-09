df600fa31674ed20dd3cfbd38ad937ae87f44229
Elnix90 <elnix91@gmail.com>
2025-11-09 08:47:40 +0100

    Updated to 0.8-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.elnix.notes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.6-alpha"
+        versionCode = 8
+        versionName = "0.8-alpha"
     }
 
     buildTypes {

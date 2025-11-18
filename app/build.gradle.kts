2292ed7ee93e158ccfabd79972bc12d11a687f5c
Elnix90 <elnix91@gmail.com>
2025-11-18 19:39:22 +0100

    Updated to v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.elnix.notes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.0.0"
+        versionCode = 16
+        versionName = "1.1.0"
     }
 
 
